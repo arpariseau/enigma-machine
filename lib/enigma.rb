0fe7607db2142758f04d157670e1fc9243db2f2a
Alex Pariseau <arpariseau@gmail.com>
2020-04-18 15:31:00 -0600

    Can split message into 4 chunks

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,27 @@
 class Enigma
+  attr_reader :cipher_a, :cipher_b, :cipher_c, :cipher_d
+
+  def initialize
+    @cipher_a = []
+    @cipher_b = []
+    @cipher_c = []
+    @cipher_d = []
+  end
+
+  def encrypt(message, keycode, date)
+    split_message(message)
+    #create the offsets
+    #adjust the messages
+    #concatenate
+  end
+
+  def split_message(message)
+    message.chars.each_slice(4) do |fourletter|
+      @cipher_a << fourletter[0]
+      @cipher_b << fourletter[1]
+      @cipher_c << fourletter[2]
+      @cipher_d << fourletter[3]
+    end
+  end
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -12,4 +12,25 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, @enigma
   end
 
+  def test_attributes
+    assert_equal [], @enigma.cipher_a
+    assert_equal [], @enigma.cipher_b
+    assert_equal [], @enigma.cipher_c
+    assert_equal [], @enigma.cipher_d
+  end
+
+  def test_encrypt
+    skip
+    expected = {encryption: "keder ohulw", key: "02715", date: "040895"}
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_split_message
+    @enigma.split_message("Hello, world")
+    assert_equal ["H", "o", "o"], @enigma.cipher_a
+    assert_equal ["e", ",", "r"], @enigma.cipher_b
+    assert_equal ["l", " ", "l"], @enigma.cipher_c
+    assert_equal ["l", "w", "d"], @enigma.cipher_d
+  end
+
 end
