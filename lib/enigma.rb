cddd82944426137a8778f306e228474653d57bed
Alex Pariseau <arpariseau@gmail.com>
2020-04-18 16:19:42 -0600

    Can now create the 4 different offsets from code and date

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,25 +2,41 @@ class Enigma
   attr_reader :cipher_a, :cipher_b, :cipher_c, :cipher_d
 
   def initialize
-    @cipher_a = []
-    @cipher_b = []
-    @cipher_c = []
-    @cipher_d = []
+    @cipher_a = Hash.new(0)
+    @cipher_b = Hash.new(0)
+    @cipher_c = Hash.new(0)
+    @cipher_d = Hash.new(0)
   end
 
   def encrypt(message, keycode, date)
-    split_message(message)
-    #create the offsets
+    messages = split_message(message)
+    offsets = create_shifts(keycode, date)
+    create_ciphers(messages, offsets)
     #adjust the messages
     #concatenate
   end
 
   def split_message(message)
+    msg_a, msg_b, msg_c, msg_d = [], [], [], []
     message.chars.each_slice(4) do |fourletter|
-      @cipher_a << fourletter[0]
-      @cipher_b << fourletter[1]
-      @cipher_c << fourletter[2]
-      @cipher_d << fourletter[3]
+      msg_a << fourletter[0]; msg_b << fourletter[1]
+      msg_c << fourletter[2]; msg_d << fourletter[3]
+    end
+    [msg_a.join, msg_b.join,msg_c.join, msg_d.join]
+  end
+
+  def create_shifts(keycode, date)
+    keyshifts = [keycode[0..1].to_i, keycode[1..2].to_i,
+                 keycode[2..3].to_i, keycode[3..4].to_i]
+    create_offsets(keyshifts, date)
+  end
+
+  def create_offsets(keyshifts, date)
+    date_squared = (date.to_i ** 2).to_s[-4..-1]
+    i = -1
+    keyshifts.map do |keyshift|
+      i += 1
+      keyshift += date_squared[i].to_i
     end
   end
 
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -13,10 +13,10 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_attributes
-    assert_equal [], @enigma.cipher_a
-    assert_equal [], @enigma.cipher_b
-    assert_equal [], @enigma.cipher_c
-    assert_equal [], @enigma.cipher_d
+    assert_equal ({}), @enigma.cipher_a
+    assert_equal ({}), @enigma.cipher_b
+    assert_equal ({}), @enigma.cipher_c
+    assert_equal ({}), @enigma.cipher_d
   end
 
   def test_encrypt
@@ -26,11 +26,15 @@ class EnigmaTest < Minitest::Test
   end
 
   def test_split_message
-    @enigma.split_message("Hello, world")
-    assert_equal ["H", "o", "o"], @enigma.cipher_a
-    assert_equal ["e", ",", "r"], @enigma.cipher_b
-    assert_equal ["l", " ", "l"], @enigma.cipher_c
-    assert_equal ["l", "w", "d"], @enigma.cipher_d
+    assert_equal ["Hoo", "e,r", "l l", "lwd"], @enigma.split_message("Hello, world")
+  end
+
+  def test_create_shifts
+    assert_equal [3, 27, 73, 20], @enigma.create_shifts("02715", "040895")
+  end
+
+  def test_create_offsets
+    assert_equal [3, 27, 73, 20], @enigma.create_offsets([02, 27, 71, 15], "040895")
   end
 
 end
