2e076e0b2671a6505bdf9a734716246a92a63ee7
Alex Pariseau <arpariseau@gmail.com>
2020-04-19 16:50:07 -0600

    Can create a cipher from a shift. Fixed encode to accommodate non-encoded letters

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -7,7 +7,22 @@ class Cipher
 
   def encode(message, shift)
     create_cipher(shift)
-    message.downcase.chars.map {|letter| @cipher[letter]}.join
+    message.downcase.chars.map do |letter|
+      if @cipher.include?(letter)
+        @cipher[letter]
+      else
+        letter
+      end
+    end.join
+  end
+
+  def create_cipher(shift)
+    shift -= 27 until shift <= 27
+    base = ("a".."z").to_a << " "
+    shifted = base.dup
+    shifting = shifted.shift(shift)
+    shifted += shifting
+    @cipher = Hash[base.zip shifted]
   end
 
 end
--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -16,7 +16,21 @@ class CipherTest < Minitest::Test
   end
 
   def test_encode
-    assert_equal "lbkr", @cipher.encode("Duck", 7)
+    assert_equal "kajr?", @cipher.encode("Duck?", 7)
+  end
+
+  def test_create_cipher
+    expected = {"a" => "e", "b" => "f", "c" => "g",
+                "d" => "h", "e" => "i", "f" => "j",
+                "g" => "k", "h" => "l", "i" => "m",
+                "j" => "n", "k" => "o", "l" => "p",
+                "m" => "q", "n" => "r", "o" => "s",
+                "p" => "t", "q" => "u", "r" => "v",
+                "s" => "w", "t" => "x", "u" => "y",
+                "v" => "z", "w" => " ", "x" => "a",
+                "y" => "b", "z" => "c", " " => "d"}
+    @cipher.create_cipher(31)
+    assert_equal expected, @cipher.cipher
   end
 
 end
