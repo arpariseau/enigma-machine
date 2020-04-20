0d933e00135c212b375ca7479f01f549f16ff7dd
Alex Pariseau <arpariseau@gmail.com>
2020-04-20 10:02:48 -0600

    Instantiation for decryption CLI

---
+++ test/decryption_test.rb
@@ -0,0 +1,13 @@
+require_relative 'test_helper'
+require './lib/decryption'
+
+class DecryptionTest < Minitest::Test
+
+  def setup
+    @decrypt = Decryption.new
+  end
+
+  def test_existence
+    assert_instance_of Decryption, @decrypt
+  end
+end
