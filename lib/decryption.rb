9345cf1dd99a0ca988dee23a09d1d6abbda3f119
Alex Pariseau <arpariseau@gmail.com>
2020-04-20 10:07:49 -0600

    Now has instance variables

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -1,3 +1,12 @@
 class Decryption
+  attr_reader :input_path, :output_path,
+              :keycode, :date
+
+  def initialize
+    @input_path = ""
+    @output_path = ""
+    @keycode = ""
+    @date = ""
+  end
 
 end
--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -11,4 +11,11 @@ class DecryptionTest < Minitest::Test
     assert_instance_of Decryption, @decrypt
   end
 
+  def test_attributes
+    assert_equal "", @decrypt.input_path
+    assert_equal "", @decrypt.output_path
+    assert_equal "", @decrypt.keycode
+    assert_equal "", @decrypt.date
+  end
+
 end
