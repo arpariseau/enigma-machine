3c80588b53d33cb7c52e4619fa9a1be112c1fd3d
Alex Pariseau <arpariseau@gmail.com>
2020-04-19 18:57:12 -0600

    Add file reading and writing to modules, as they're likely to be shared between encrypt, decrypt and crack

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,7 +1,10 @@
 require_relative 'enigma'
+require_relative 'readable'
+require_relative 'writable'
 
 class Encrypt
-attr_reader :input_path, :output_path
+  include Readable, Writable
+  attr_reader :input_path, :output_path
 
   def initialize
     @input_path = ""
@@ -17,21 +20,11 @@ attr_reader :input_path, :output_path
     @output_path = get_paths[1]
   end
 
-  def read_message
-    File.open(@input_path).readlines.map {|line| line.chomp}.join
-  end
-
   def encrypt_message
     enigma = Enigma.new
     encrypted = enigma.encrypt(read_message)
     puts "Created '#{@output_path}' with the key #{encrypted[:key]} and date #{encrypted.date}"
     write_message(encrpyted[:encryption])
-  end
-
-  def write_message(message)
-    cipher_msg = File.open(@output_path)
-    cipher_msg.write(message)
-    cipher_msg.close
   end
 
 end
