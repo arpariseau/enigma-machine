d12f0c69c5a7f6d2c0df8257381b20b8a19d7bf7
Alex Pariseau <arpariseau@gmail.com>
2020-04-20 10:22:33 -0600

    Split out runner file for encryption, as well

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,34 +1,4 @@
-require_relative 'enigma'
-require_relative 'readable'
-require_relative 'writable'
-
-class Encrypt
-  include Readable, Writable
-  attr_reader :input_path, :output_path
-
-  def initialize
-    @input_path = ""
-    @output_path = ""
-  end
-
-  def get_paths
-    ARGV
-  end
-
-  def set_paths
-    @input_path = get_paths[0]
-    @output_path = get_paths[1]
-  end
-
-  def encrypt_message
-    enigma = Enigma.new
-    encrypted = enigma.encrypt(read_message)
-    puts "Created '#{@output_path}' with the key #{encrypted[:key]} and date #{encrypted[:date]}"
-    write_message(encrypted[:encryption])
-  end
-
-end
-
+require_relative "encryption"
 encrypt = Encrypt.new
 encrypt.set_paths
 encrypt.encrypt_message
